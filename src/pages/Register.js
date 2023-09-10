6db8ac00f27fe08ee880fd9b3470cadd63414847
Syed Javith <syedjavith14@gmail.com>
2023-09-10 19:11:12 +0530

    fianl

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { url } from '../url';
 
 const Register = () => {
   const navigate = useNavigate();
